55e8779b42293582b02cee575c64e595a172b26f
Son Do <dominhson580@gmail.com>
2022-09-06 10:20:06 +0700

    Create a default list

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ function nodeFactory(value = null, next = null) {
 }
 
 function linkedListFactory() {
+  const list = {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: "a",
+        next: null,
+      },
+    },
+  };
+
   const append = (value) => {
     const node = nodeFactory(value);
   };
+
+  return { append };
 }
